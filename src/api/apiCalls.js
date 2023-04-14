59c8cfd59d9b004878abfaa60738a2a9e725c04b
Berke Gvn <berkegvn@gmail.com>
2023-04-14 16:35:00 +0300

    api calls

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,9 +1,16 @@
 async function fetchGifs() {
+  const urls = []
+
+  for (let i = 0; i < 10; i++) {
+    const url = import.meta.env.VITE_SOME_KEY;
+    urls.push(url);
+  }
+
   try {
-    const response = await fetch(import.meta.env.VITE_SOME_KEY)
-    const data = await response.json()
-    console.log(data.data.images.original.url)
-    return data.data.images.fixed_height.url
+    const responses = await Promise.all(urls.map(url => fetch(url)));
+    const data = await Promise.all(responses.map(response => response.json()));
+    const gifs = data.map(item => item.data.images.fixed_height.url);
+    return gifs;
   }
   catch (error) {
     console.error('ERRRR SOMETHING WENT WRONG')
