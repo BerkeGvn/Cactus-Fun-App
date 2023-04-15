b249a57aedca9720aea7748ccab9c54728ea303e
Berke Gvn <berkegvn@gmail.com>
2023-04-15 23:29:45 +0300

    -script part-

--- src/View/InfinitePage.vue
+++ src/View/InfinitePage.vue
@@ -2,27 +2,61 @@
   <ul>
     <li v-for="(content, index) in contents" :key="index">
       <img :src="content.gif" alt="" />
-      <p>{{ content.joke }}</p>
+      <div class="joke">
+        <p>{{ content.joke.setup }}</p>
+        <p>{{ content.joke.delivery }}</p>
+      </div>
     </li>
   </ul>
+
+  <h1 v-if="isLoading">LOADING...</h1>
+  <InstersectionObserver @observe="loadMore"></InstersectionObserver>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, watch } from 'vue'
 import { fetchGifs, fetchJokes } from '../api/apiCalls'
+import InstersectionObserver from '../components/InstersectionObserver.vue'
 
 const contents = ref([])
+const isLoading = ref(null)
+const page = ref(0)
 
-onMounted(async () => {
+// I am thinking about adding contact info in the list, might delete it
+watch(page, (number) => {
+  if (number % 3 === 0) {
+    contents.value.push({
+      gif: 'https://avatars.githubusercontent.com/u/62262384?v=4',
+      joke: { setup: 'Berke Guven', delivery: 'Github' }
+    })
+  }
+})
+
+async function fetchData() {
+  isLoading.value = true
   const gifData = await fetchGifs()
   const jokeData = await fetchJokes()
   for (let i = 0; i < 10; i++) {
     contents.value.push({
-      gif: gifData[i],
-      joke: jokeData[i].setup + ' ' + jokeData[i].delivery
+      gif: [gifData[i]],
+      joke: { setup: jokeData[i].setup, delivery: jokeData[i].delivery }
     })
   }
-})
+  isLoading.value = false
+  page.value += 1
+  console.log(page.value)
+}
+
+function loadMore() {
+  fetchData()
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+li {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-bottom: 2rem;
+}
+</style>
---
+++ src/components/InstersectionObserver.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="sentinel" ref="sentinel"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const sentinel = ref(null)
+const emit = defineEmits(['observe'])
+
+onMounted(() => {
+  const options = {
+    margin: '20px'
+  }
+  const pageObserver = new IntersectionObserver(([entry]) => {
+    if (entry.isIntersecting) {
+      emit('observe')
+      console.log('observing')
+    }
+  }, options)
+  pageObserver.observe(sentinel.value)
+})
+</script>
+
+<style lang="scss" scoped></style>
