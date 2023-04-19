a677f04ce1daf567dbc39e4451a736b87450ac0d
Berke Guven <62262384+BerkeGvn@users.noreply.github.com>
2023-04-19 17:05:42 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'Cactus-Fun-App/',
+  base: '/Cactus-Fun-App/',
   plugins: [vue()],
   resolve: {
     alias: {
